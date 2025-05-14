abe08f8e9553e8d6d1776353c773b04b307363d0
S SIDHARTH <sidharthpunalur@gmail.com>
2025-05-14 22:01:31 +0530

    test route added

--- mument/urls.py
+++ mument/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin', admin.site.urls),
+    path('api/profile', include('user_profile.urls')), 
 ]
