97758489522873cd56c456cd530729c8fe2175f1
S SIDHARTH <sidharthpunalur@gmail.com>
2025-06-04 11:02:40 +0530

    cors and allowed host added and also connected to db

--- mument/urls.py
+++ mument/urls.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Mument API",
+      default_version='v0',
+      description="Django REST Framework API for Mument",
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-    path('admin', admin.site.urls),
-    path('api/profile', include('user_profile.urls')), 
+    path('admin/', admin.site.urls),
+    path('api/profile', include('user_profile.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
