5a28bfd56a398c80f2aba8aa9766f6810854372a
S SIDHARTH <sidharthpunalur@gmail.com>
2025-06-16 21:08:25 +0530

    created time in checkpoints added

--- checkpoint/models.py
+++ checkpoint/models.py
@@ -7,6 +7,7 @@ class Checkpoint(models.Model):
     description = models.TextField()
     title = models.CharField(max_length=255)
     image_url = models.URLField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True) 
     class Meta:
         db_table = "checkpoints"
     def __str__(self):
